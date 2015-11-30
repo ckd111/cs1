9f65cd0d03a4367f8a4371d495bc99a44d181242
Caroline Kelly <cek3ry@virginia.edu>
2015-11-29 19:47:52 -0500

    Image Processing App

---
+++ code/python/imageproc/image_processing.py
@@ -0,0 +1,160 @@
+import pygame as pg
+import numpy
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel
+def grayPixel(pixel):
+    red_intensity = pixel[0]
+    green_intensity = pixel[1]
+    blue_intensity = pixel[2]
+    ave_intensity = (red_intensity + green_intensity+ blue_intensity)//3
+    return (ave_intensity, ave_intensity, ave_intensity)
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+def intensify(pixel,quantity):
+    return (pixel[0]+quantity, pixel[1]+quantity, pixel[2]+quantity)
+
+
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
+
+def bw(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = grayPixel(pixels3d[x,y])
+
+def lighten(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = lighten_pixel(pixels3d[x,y])
+
+def lighten_pixel(pixel):
+    r = numpy.int32(pixel[0]) + 10
+    if r > 255:
+        r = 255
+    g = numpy.int32(pixel[1]) + 10
+    if g > 255:
+        g = 255
+    b = numpy.int32(pixel[2]) + 10
+    if b > 255:
+        b = 255
+    return (numpy.int8(r), numpy.int8(g), numpy.int8(b))
+
+def darken(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = darken_pixel(pixels3d[x,y])
+
+def darken_pixel(pixel):
+    r = numpy.int32(pixel[0]) - 10
+    if r < 0:
+        r = 0
+    g = numpy.int32(pixel[1]) - 10
+    if g < 0:
+        g = 0
+    b = numpy.int32(pixel[2]) - 10
+    if b < 0:
+        b = 0
+    return (numpy.int8(r), numpy.int8(g), numpy.int8(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
